0733fab374caddc1179c9b2917e8f2d8c9072cea
Siegfried Ludwig <jay.cr4y@gmail.com>
2018-06-15 12:18:33 +0200

    backup LSA code

--- is_constructs.py
+++ is_constructs.py
@@ -10,39 +10,43 @@ import pandas as pd
 import csv
 import matplotlib.pyplot as plt
 
-# Load data.
-file = r'LarsenBong2016GoldStandard.xls'
-gold_items = pd.read_excel(file, sheet_name='Items')
-gold_standard = pd.read_excel(file, sheet_name='GoldStandard')
-variableIDs = sorted(list(set(gold_items['VariableId'])))
-item_corpus = np.asarray(gold_items['Text'])
 
-# Prepare document-term matrices.
-# TODO: stem words (?)
-count_vectorizer = CountVectorizer(stop_words='english', lowercase=True, dtype='int32')
-dt_matrix = count_vectorizer.fit_transform(item_corpus)
-dt_matrix = dt_matrix.toarray()
-dt_matrix_l2 = Normalizer(copy=True, norm='l2').fit_transform(dt_matrix)
-tfidf_vectorizer = TfidfVectorizer(stop_words='english', lowercase=True, norm='l2', use_idf=True, smooth_idf=True)
-dt_matrix_tfidf_l2 = tfidf_vectorizer.fit_transform(item_corpus)
-dt_matrix_tfidf_l2 = dt_matrix_tfidf_l2.toarray()
-feature_names = count_vectorizer.get_feature_names()
-print("Document-term matrices prepared (items x terms).")
-# print(pd.DataFrame(dt_matrix, index=item_corpus, columns=count_vectorizer.get_feature_names()).head(5))
+def create_dt_matrices():
+    """Creates and returns a dictionary of document-term matrices with different processing methods.
+    Also returns the feature names (terms) extracted by the vectorizer."""
+    # TODO: stem words (?)
+    count_vectorizer = CountVectorizer(stop_words='english', lowercase=True, dtype='int32')
+    dt_matrix = count_vectorizer.fit_transform(item_corpus)
+    dt_matrix = dt_matrix.toarray()
+    dt_matrix_l2 = Normalizer(copy=True, norm='l2').fit_transform(dt_matrix)
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english', lowercase=True, norm='l2', use_idf=True, smooth_idf=True)
+    dt_matrix_tfidf_l2 = tfidf_vectorizer.fit_transform(item_corpus)
+    dt_matrix_tfidf_l2 = dt_matrix_tfidf_l2.toarray()
+    feature_names = count_vectorizer.get_feature_names()
+    # print(pd.DataFrame(dt_matrix, index=item_corpus, columns=count_vectorizer.get_feature_names()).head(5))
 
-# Apply log entropy and L2 normalization to count matrix.
-# https://radimrehurek.com/gensim/models/logentropy_model.html
-# Implementation checked manually.
-local_weight_matrix = np.log(dt_matrix + 1)
-p_matrix = np.divide(dt_matrix, np.tile(np.sum(dt_matrix, axis=0), (len(dt_matrix), 1)))
-log_p_matrix = np.log(p_matrix)
-log_p_matrix[np.isneginf(log_p_matrix)] = 0
-global_weight_matrix = np.tile(1 + np.divide(np.sum(np.multiply(p_matrix, log_p_matrix),
-                                                    axis=0), np.log(len(dt_matrix) + 1)), (len(dt_matrix), 1))
-final_weight_matrix = np.multiply(local_weight_matrix, global_weight_matrix)
-dt_matrix_log = np.multiply(dt_matrix, final_weight_matrix)
-dt_matrix_log_l2 = Normalizer(copy=True, norm='l2').fit_transform(dt_matrix_log)
-print("Document-term count matrix normalized with log entropy and L2 norm.")
+    # Apply log entropy and L2 normalization to count matrix.
+    # https://radimrehurek.com/gensim/models/logentropy_model.html
+    # Implementation checked manually.
+    local_weight_matrix = np.log(dt_matrix + 1)
+    p_matrix = np.divide(dt_matrix, np.tile(np.sum(dt_matrix, axis=0), (len(dt_matrix), 1)))
+    log_p_matrix = np.log(p_matrix)
+    log_p_matrix[np.isneginf(log_p_matrix)] = 0
+    global_weight_matrix = np.tile(1 + np.divide(np.sum(np.multiply(p_matrix, log_p_matrix),
+                                                        axis=0), np.log(len(dt_matrix) + 1)), (len(dt_matrix), 1))
+    final_weight_matrix = np.multiply(local_weight_matrix, global_weight_matrix)
+    dt_matrix_log = np.multiply(dt_matrix, final_weight_matrix)
+    dt_matrix_log_l2 = Normalizer(copy=True, norm='l2').fit_transform(dt_matrix_log)
+    print("Document-term matrices prepared (items x terms).")
+
+    dt_matrices = {
+        'dt_matrix': dt_matrix,
+        'dt_matrix_l2': dt_matrix_l2,
+        'dt_matrix_log': dt_matrix_log,
+        'dt_matrix_log_l2': dt_matrix_log_l2,
+        'dt_matrix_tfidf_l2': dt_matrix_tfidf_l2,
+    }
+    return feature_names, dt_matrices
 
 
 def recreate_construct_identity_gold():
@@ -63,15 +67,15 @@ def recreate_construct_identity_gold():
     return construct_identity_gold
 
 
-def item_vectors_svd(dt_matrix):
+def item_vectors_svd(dtm_identifier):
     """Create item vectors with SVD a.k.a. LSA."""
     t_svd = TruncatedSVD(n_components=300, algorithm='randomized')
-    item_vectors = t_svd.fit_transform(dt_matrix)
+    item_vectors = t_svd.fit_transform(dt_matrices[dtm_identifier])
     print("LSA item vectors created with SVD.")
     return item_vectors
 
 
-def item_vectors_glove(dt_matrix, sub_dict_file, full_dict_file=None):
+def item_vectors_glove(dtm_identifier, sub_dict_file, denominator=None, full_dict_file=None):
     """Translate items into pre-trained GloVe vector space and returns the matrix of item vectors. Sums term
     vectors of terms in item with passed document-term matrix as weighting factor. Pass file of relevant dictionary
     or pass dict_file=None and pass the file of the full vector dictionary to create the sub-dictionary."""
@@ -104,9 +108,11 @@ def item_vectors_glove(dt_matrix, sub_dict_file, full_dict_file=None):
 
     # Translate items into GloVe vectors.
     # Implementation checked manually, but there are multiple ways of doing this.
+    dt_matrix = dt_matrices[dtm_identifier]
     item_vectors = np.zeros([len(dt_matrix), len(next(iter(glove_vector_dict.values())))])
     for row_ind in range(len(dt_matrix)):
-        ctr_oov_occurrences = 0
+        ctr_oov = 0
+        value_oov = 0
         for col_ind in range(len(dt_matrix[0])):
             if np.nonzero(dt_matrix[row_ind, col_ind]):
                 try:
@@ -114,10 +120,15 @@ def item_vectors_glove(dt_matrix, sub_dict_file, full_dict_file=None):
                                                    np.asarray(glove_vector_dict[feature_names[col_ind]])
                                                    * dt_matrix[row_ind, col_ind])
                 except KeyError:
-                    ctr_oov_occurrences += dt_matrix[row_ind, col_ind]
-        # The following line could be used to average if the passed dt_matrix is not normed, but it produces some
-        # NaN entries.
-        # item_vectors[row_ind] = item_vectors[row_ind] / (np.sum(dt_matrix[row_ind]) - ctr_oov_occurrences)
+                    ctr_oov += 1
+                    value_oov += dt_matrix[row_ind, col_ind]
+        # TODO: Some error, probably created in the following lines. Test this part of the function.
+        item_vectors[row_ind] = {
+            'sum': item_vectors[row_ind] / (np.sum(dt_matrix[row_ind])),
+            'sum_value-oov': item_vectors[row_ind] / (np.sum(dt_matrix[row_ind]) - value_oov),
+            'norm': item_vectors[row_ind] / (np.linalg.norm(dt_matrix[row_ind])),
+            'norm_value-oov': item_vectors[row_ind] / (np.linalg.norm(dt_matrix[row_ind]) - value_oov)
+        }.get(denominator, item_vectors[row_ind])
         if row_ind % 100 == 0:
             print("Translating items into GloVe vectors.", (row_ind + 1) / len(dt_matrix) * 100, "%", end="\r")
     return item_vectors
@@ -172,23 +183,49 @@ def evaluate(construct_similarity):
     return fpr, tpr, roc_auc
 
 
-# Load or compute construct similarity matrix for LSA
-# TODO: produces some NaN entries in item_vectors
+# Load data.
+file = r'LarsenBong2016GoldStandard.xls'
+gold_items = pd.read_excel(file, sheet_name='Items')
+gold_standard = pd.read_excel(file, sheet_name='GoldStandard')
+variableIDs = sorted(list(set(gold_items['VariableId'])))
+item_corpus = np.asarray(gold_items['Text'])
+
+# Create document-term matrices.
+feature_names, dt_matrices = create_dt_matrices()
+
+# Load or compute construct similarity matrix for LSA.
+# TODO: produced some NaN entries in item_vectors
 try:
     construct_similarity_LSA = np.loadtxt('construct_similarity_LSA.txt')
 except FileNotFoundError:
-    construct_similarity_LSA = np.nan_to_num(compute_construct_similarity(item_vectors_svd(dt_matrix_l2)))
+    construct_similarity_LSA = np.nan_to_num(compute_construct_similarity(
+        item_vectors_svd(dt_matrices['dt_matrix_tfidf_l2'])))
     np.savetxt('construct_similarity_LSA.txt', construct_similarity_LSA)
 
-# Load or compute construct similarity matrix for GloVe
-# TODO: produces some NaN entries in item_vectors
+# Load or compute construct similarity matrix for GloVe.
+# TODO: produced some NaN entries in item_vectors
 try:
     construct_similarity_GloVe = np.loadtxt('construct_similarity_GloVe.txt')
 except FileNotFoundError:
     construct_similarity_GloVe = np.nan_to_num(compute_construct_similarity(
-        item_vectors_glove(dt_matrix, sub_dict_file='GloVe_vector_dict_6B_50d.npy',
+        item_vectors_glove(dt_matrices['dt_matrix_tfidf_l2'], sub_dict_file='GloVe_vector_dict_6B_50d.npy',
                            full_dict_file='glove.6B/glove.6B.50d.txt')))
     np.savetxt('construct_similarity_GloVe.txt', construct_similarity_GloVe)
+
+# Search for best GloVe document projection
+denominator_options = [None, 'sum', 'sum_value-oov', 'norm', 'norm_value-oov']
+# grid = [[mat, den] for mat in dt_matrices for den in denominator_options]
+grid = [['dt_matrix', None], ['dt_matrix', 'sum']]
+glove_results = []
+ctr = 0
+print("Performing grid search on GloVe.")
+for dtm_identifier, denominator in grid:
+    item_vectors_glove(dtm_identifier, denominator=denominator, sub_dict_file='GloVe_vector_dict_6B_100d.npy')
+    construct_similarity_GloVe = np.nan_to_num(compute_construct_similarity(item_vectors_glove))
+    fpr_glove, tpr_glove, roc_auc_glove = evaluate(construct_similarity_GloVe)
+    glove_results.append([dtm_identifier, denominator, roc_auc_glove])
+    ctr += 1
+    print("Grid search on GloVe.", ctr / len(grid) * 100, "%")
 
 # construct_similarity_LSA = pd.DataFrame(construct_similarity_LSA, index=variableIDs, columns=variableIDs)
 # construct_similarity_GloVe = pd.DataFrame(construct_similarity_GloVe, index=variableIDs, columns=variableIDs)
